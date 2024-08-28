d7f3971557f5f6042cfea4c968dfd112b71afd96
m0l1naa <jose.molina020925@gmail.com>
2024-08-28 11:18:58 -0600

    Creación de las clases con sus respectivos atributos

---
+++ SchoolManagementSystem/src/GestorAcademico.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GestorAcademico {
+    private ArrayList<Estudiante> estudiantes = new ArrayList<>();
+    private ArrayList<Curso> cursos = new ArrayList<>();
+    private HashMap<Integer, ArrayList<Estudiante>> estudiantesPorCurso = new HashMap<>();
+}
